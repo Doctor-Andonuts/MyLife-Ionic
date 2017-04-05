1a167698bf7c0d3058d767e4b3c12361be6589e5
Docot Andonuts <gowingjm@gmail.com>
2017-04-05 16:30:10 -0400

    working on form input saving

--- src/pages/create-tracker/create-tracker.ts
+++ src/pages/create-tracker/create-tracker.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import {Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { NavController, ViewController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 /*
   Generated class for the CreateTracker page.
@@ -13,9 +14,11 @@ import { NavController, ViewController } from 'ionic-angular';
   templateUrl: 'create-tracker.html'
 })
 export class CreateTrackerPage {
-  private createTracker : FormGroup;
+  private createTracker: FormGroup;
+  tracker: any;
 
   constructor(
+    storage: Storage,
     public navCtrl: NavController,
     public viewCtrl: ViewController,
     private formBuilder: FormBuilder
@@ -32,7 +35,13 @@ export class CreateTrackerPage {
   }
 
   submitForm() {
-    console.log(this.createTracker)
-  }
+    console.log(this.createTracker.value)
+    tracker.name = this.createTracker.value.name
+    tracker.label = this.createTracker.value.label
+    tracker.type = this.createTracker.value.type
 
+    storage.ready().then(() => {
+      storage.set('trackerMeta', tracker);
+    });
+  }
 }
